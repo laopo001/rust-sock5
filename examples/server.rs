5b29c40cd4c8b6d6977e1f6a36349d5f3371e5ea
laopo001 <353272497@qq.com>
2022-06-16 20:01:20 +0800

    add terminal clap prase

--- examples/server.rs
+++ examples/server.rs
@@ -4,6 +4,7 @@ use async_std::io::{self, ReadExt};
 use async_std::net::{TcpListener, TcpStream};
 use async_std::prelude::*;
 use async_std::task;
+use clap::Parser;
 use dns_lookup::{lookup_addr, lookup_host};
 use std::io::{Error, ErrorKind, Result};
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
@@ -12,7 +13,6 @@ use test_r::util::{self, alias, create_secret_public, get_publickey, resolve_up_
 
 async fn accept(mut stream: TcpStream) -> std::io::Result<()> {
     let mut connect = Connect::new(stream);
-   
 
     // 交换key
     let (secret, public) = create_secret_public();
@@ -21,8 +21,7 @@ async fn accept(mut stream: TcpStream) -> std::io::Result<()> {
     let shared_secret = secret.diffie_hellman(&get_publickey(remote_public));
     // dbg!(&shared_secret.as_bytes());
     connect.set_crypto(Box::new(CryptoProxy::new(shared_secret.as_bytes())));
- 
- 
+
     let (ip, port, host) = resolve_up_ip_port(&mut connect).await?;
     let addr = connect.stream.local_addr()?;
     println!("{} => {}:{} host:{}", addr, &ip, &port, &host);
@@ -39,8 +38,8 @@ async fn accept(mut stream: TcpStream) -> std::io::Result<()> {
     Ok(())
 }
 
-async fn app() -> std::io::Result<()> {
-    let listener = TcpListener::bind("0.0.0.0:9998").await?;
+async fn app(args: Args) -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:".to_string() + &args.port.to_string()).await?;
     let mut incoming = listener.incoming();
 
     while let Some(stream) = incoming.next().await {
@@ -50,8 +49,19 @@ async fn app() -> std::io::Result<()> {
     Ok(())
 }
 
+/// 服务端
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    /// 请输入端口
+    #[clap(short, long, value_parser, default_value_t = 8877)]
+    port: u16,
+}
+
 fn main() {
+    let args = Args::parse();
+    dbg!(&args);
     // log4rs::init_file("/home/ldh/Projects/test-r/log4rs.server.yaml", Default::default()).unwrap();
 
-    task::block_on(app());
+    task::block_on(app(args));
 }
